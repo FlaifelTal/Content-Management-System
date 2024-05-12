48c5aeb5eccebd0e8df81e85adb3fd40d73d445c
Tala Flaifel <flaifeltala@gmail.com>
2024-05-12 11:42:56 +0300

    new file:   app/Http/Controllers/aboutUsController.php 	new file:   app/Http/Controllers/albumController.php 	new file:   app/Http/Controllers/contactUsController.php 	new file:   app/Http/Controllers/homeController.php 	new file:   app/Http/Controllers/portfolioController.php 	new file:   app/Http/Controllers/postcontroller.php 	new file:   app/Http/Controllers/privacyController.php 	new file:   app/Http/Controllers/servicesController.php 	new file:   app/Http/Controllers/termsController.php 	modified:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Http/Requests/contactRequest.php 	new file:   app/Models/category.php 	new file:   app/Models/post.php 	new file:   database/migrations/2024_05_05_103719_create_postTest_table.php 	new file:   database/migrations/2024_05_05_105224_create_test_table.php 	new file:   database/migrations/2024_05_05_105554_create_test2_table.php 	new file:   database/migrations/2024_05_05_110551_add_postTest_table.php 	new file:   database/migrations/2024_05_05_150437_create_posts_table.php 	new file:   database/migrations/2024_05_05_150605_create_categories_table.php 	new file:   database/migrations/2024_05_11_230329_add_posts_table.php 	modified:   database/seeders/DatabaseSeeder.php 	new file:   database/seeders/postSeeder.php 	new file:   public/assets/css/bootstrap.min.css 	new file:   public/assets/css/style.css 	new file:   public/assets/img/pic1.jpg 	new file:   public/assets/img/pic10.jpg 	new file:   public/assets/img/pic11.jpg 	new file:   public/assets/img/pic12.jpg 	new file:   public/assets/img/pic13.jpg 	new file:   public/assets/img/pic14.jpg 	new file:   public/assets/img/pic15.png 	new file:   public/assets/img/pic2.jpg 	new file:   public/assets/img/pic6.jpg 	new file:   public/assets/img/pic7.jpg 	new file:   public/assets/img/pic8.jpg 	new file:   public/assets/img/pic9.jpg 	new file:   public/assets/js/bootstrap.bundle.min.js 	new file:   public/assets/js/main.js 	new file:   resources/views/aboutUs.blade.php 	new file:   resources/views/album.blade.php 	new file:   resources/views/contact.blade.php 	new file:   resources/views/create.blade.php 	new file:   resources/views/edit.blade.php 	new file:   resources/views/home.blade.php 	new file:   resources/views/layouts/footer.blade.php 	new file:   resources/views/layouts/header.blade.php 	new file:   resources/views/layouts/master.blade.php 	new file:   resources/views/portfolio.blade.php 	new file:   resources/views/privacy.blade.php 	new file:   resources/views/services.blade.php 	new file:   resources/views/show.blade.php 	new file:   resources/views/terms.blade.php 	new file:   resources/views/trashed.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use App\Http\Controllers\aboutUsController;
+use App\Http\Controllers\albumController;
+use App\Http\Controllers\contactUsController;
+use App\Http\Controllers\homeController;
+use App\Http\Controllers\portfolioController;
+use App\Http\Controllers\postcontroller;
+use App\Http\Controllers\privacyController;
+use App\Http\Controllers\servicesController;
+use App\Http\Controllers\termsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +23,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('home',[homeController::class,'index'])->name('home');
+Route::get('about',[aboutUsController::class,'index'])->name('aboutUs');
+Route::get('album',[albumController::class,'index'])->name('album');
+Route::get('contact',[contactUsController::class,'index'])->name('contact');
+Route::POST('contact',[contactUsController::class,'handlecontact'])->name('contact.submit');
+
+Route::get('portfolio',[portfolioController::class,'index'])->name('portfolio');
+Route::get('privacy',[privacyController::class,'index'])->name('privacy');
+Route::get('services',[servicesController::class,'index'])->name('services');
+Route::get('terms',[termsController::class,'index'])->name('terms');
+//must be before the main source route 
+Route::get('/post/trash',[postcontroller::class,'trashed'])->name('post.trashed');
+Route::get('/post/{id}/restore',[postcontroller::class,'restore'])->name('post.restore');
+Route::delete('/post/{id}/forceDelete',[postcontroller::class,'forceDelete'])->name('post.forceDelete');
+
+Route::resource('post',postcontroller::class);
